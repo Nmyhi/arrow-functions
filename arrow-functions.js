a037774da970669cb9e48a5111b5343c8bb79770
Mihaly Lovrencsics <lomysa@gmail.com>
2023-08-07 22:08:46 +0000

    Add code for representing different function types

--- arrow-functions.js
+++ arrow-functions.js
@@ -5,10 +5,41 @@
 
 // Vanilla JavaScript Function
 
+function addTwoNumbers0(a, b) {
+    
+   return a + b;
+}
+let sum0 = addTwoNumbers0(3, 5);
+console.log(sum0);
+
 // Arrow Function With Parameters
+
+const addTwoNumbers = (a, b) => {
+
+    return a + b;
+}
+ let sum = addTwoNumbers(3, 5);
+ console.log(sum);
 
 // Single Line Arrow Function With Parameters
 
+const addTwoNumbers2 = (a, b) => a + b;
+let sum2 = addTwoNumbers2(6, 4);
+console.log(sum2);
+
 // Implicit Returns
 
+const saySomething = message => console.log(message);
+saySomething("Hello There!");
+
+const sayHello = () => console.log("Hello");
+sayHello();
+
 // Returning Multiple Lines
+
+const returnMultipleLines = () => (
+    `<p>
+        This is a multiple line string!
+    </p>`
+)
+console.log(returnMultipleLines());
